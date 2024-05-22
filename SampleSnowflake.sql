d2ad4098278c780e15a4fbfb360a45a2d623feb9
abhijith285 <fergie.united285@gmail.com>
2024-05-22 18:17:44 -0400

    updated commits

--- SampleSnowflake.sql
+++ SampleSnowflake.sql
@@ -1,9 +1,6 @@
+
+--check monthly revenue
 select * from SNOWPARK_DEMO_DB.SNOWPARK_DEMO_SCHEMA.MONTHLY_REVENUE;
 
---comment
---new comment
---new
-
-select * from  dummy_table where initiative  in ('Initiative_9');
-
-
+--execute dummy stored proc
+call SNOWPARK_DEMO_DB.SNOWPARK_DEMO_SCHEMA.DummyProc();
